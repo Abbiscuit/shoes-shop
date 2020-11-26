bf497be4696fa8d5d1641b1c35fc3fb7ad6a1976
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2020-11-26 18:15:22 +0900

    Initial Commit

---
+++ screens/ProductDetail.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const ProductDetail = () => {
+  return (
+    <View>
+      <Text>Product Detail</Text>
+    </View>
+  );
+};
+
+export default ProductDetail;
+
+const styles = StyleSheet.create({});
